a66c51b1712ea1b2ed2a6a84316840ccfc67adf6
Matt Ng <66539163+armqtt@users.noreply.github.com>
2021-10-01 16:57:22 +0800

    fix layout and js problem

--- static/js/mainv2.js
+++ static/js/mainv2.js
@@ -1,6 +1,11 @@
 document.getElementById("this-year").innerHTML = new Date().getFullYear();
 
 var vid = document.getElementById("coverVideo");
+if(vid != undefined){
+    // vid.autoplay = true;
+    // vid.load();
+    vid.play();
+}
 // vid.autoplay = true;
 // vid.load();
 vid.play();
